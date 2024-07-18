e6381f30df588a87f6ffb830cc878db4a24bb560
tkusal <thiago.kusal@outlook.com>
2024-07-17 22:36:08 -0300

    AUla 17/07/2024

--- Semana 1/Atv1.c
+++ Semana 1/Atv1.c
@@ -1,3 +1,25 @@
 /*
     Escreva um programa que some dois números inteiros
 */
+
+#include <stdio.h>
+
+int main() {
+
+    int numero1;
+    int numero2;
+    int result;
+
+    printf("Insira o primeiro numero inteiro: ");
+    scanf("%d", &numero1);
+    
+    printf("Insira o segundo numero inteiro: ");
+    scanf("%d", &numero2);
+
+    result = numero1 + numero2;
+
+    printf("Ola povo do Ampliar\n");
+    printf("O resultado da soma e: %d", result);
+
+    return 0;
+}
