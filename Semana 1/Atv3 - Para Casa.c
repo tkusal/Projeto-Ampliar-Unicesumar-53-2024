5e9c113177caafc25ce46faeffa6199dc5c00b81
tkusal <thiago.kusal@outlook.com>
2024-07-23 22:11:03 -0300

    Aula 23/07/2024

--- Semana 1/Atv3 - Para Casa.c
+++ Semana 1/Atv3 - Para Casa.c
@@ -7,7 +7,15 @@
 
 int main() {
 
+    float salario, valorImposto, valorLiquido;
 
+    printf("Digite o salario: ");
+    scanf("%f", &salario);
+
+    valorImposto = salario * 25/100;
+    valorLiquido = salario - valorImposto;
+
+    printf("O imposto e de: %.2f \nO salario liquido e de: %.2f", valorImposto, valorLiquido);
     
     return 0;
 }
