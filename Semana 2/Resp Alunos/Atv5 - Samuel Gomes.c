1c38f5cf853df2ecea64d1de7d40c5ace7a23e39
svgomess <101908015+svgomess@users.noreply.github.com>
2024-07-30 11:49:43 -0300

    Add files via upload
    Respostas Semana 2

---
+++ Semana 2/Resp Alunos/Atv5 - Samuel Gomes.c
@@ -0,0 +1,81 @@
+/*
+    Escreva um algoritmo em C que receba o salário de um funcionário e o cargo de seu código.
+    De acordo com seu código, informe quanto de aumento ele teve e o valor atual de seu salário.
+    A tabela a seguir apresenta os cargos e seus respectivos aumentos:
+
+    Códigos        |         Cargo           |      Aumento
+    1                   Desenvolvedor Java           30%
+    2                   Analista de Redes            25%
+    3                   Arquiteto DevOps             50%
+    4                   Técnico de Suporte           13%
+
+    A saída deverá ser:
+    Salário atual: R$xxxxx,xx
+    Aumento de x%: R$xxx,xx
+    Novo salário:  R$xxxxx,xx
+
+    Desafio (opcional):
+    Que tal se arriscar um pouco mais em um conceito que não trabalhamos ainda? Como entrada, receba também o nome do funcionário e escrevê-lo na saída?
+
+    Se for se aventurar, a saída deverá ser assim:
+
+    Nome do Funcionário: xxxxxxxxxxx
+    Salário atual: R$xxxxx,xx
+    Aumento de x%: R$xxx,xx
+    Novo salário:  R$xxxxx,xx
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main() {
+    
+    setlocale(LC_ALL, "Portuguese");
+    
+    char Nome[100];
+    float SalarioAtual, NovoSalario, AumentoSalario;
+    int CodigoCargo, Porcentagem;
+    
+    printf("Digite o Nome do Funcionário: ");
+    // scanf("%s", Nome); // Lê somente a primeira palavra da string
+    fgets(Nome, sizeof(Nome), stdin); // Lê todas as palavras da string independente de espaços
+
+    printf("Digite o salário do Funcionário: ");
+    scanf("%f", &SalarioAtual);
+    
+    printf("Digite o código do cargo: ");
+    scanf("%d", &CodigoCargo);
+    
+    switch (CodigoCargo) {
+        
+        case 1:
+            Porcentagem = 30;
+            break;
+        case 2:
+            Porcentagem = 25;
+            break;
+        case 3:
+            Porcentagem = 50;
+            break;
+        case 4:
+            Porcentagem = 13;
+            break;
+        default:
+            printf("Código digitado inválido! \n");
+            Porcentagem = 0;
+            break;
+
+    }
+
+    AumentoSalario  = SalarioAtual * (Porcentagem / 100.0);
+    NovoSalario     = SalarioAtual + AumentoSalario;
+    
+    printf("Nome do Funcionário: Samuel Gomes \n");
+    printf("Salário atual: R$ %.2f \n", SalarioAtual);
+    printf("Aumento de %d%%: R$ %.2f \n", Porcentagem, AumentoSalario);
+    printf("Novo salário: R$ %.2f \n", NovoSalario);
+    
+    return 0;
+}
