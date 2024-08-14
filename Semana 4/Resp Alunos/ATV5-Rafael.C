6188db8771bdac192e0a233203809cdee887a33e
Rafael Silva Moreira <100470773+Kazuma01@users.noreply.github.com>
2024-08-14 00:00:23 -0300

    Create ATV5-Rafael.C
    dois em um

---
+++ Semana 4/Resp Alunos/ATV5-Rafael.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int matriz [3][3],somar = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("digite o numero da matriz :\n");
+            printf("--->");
+            scanf("%d", &matriz[i][j]);
+            if ( i == j)
+            {
+                somar += matriz[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        somar += (matriz[i][2 - 1]);
+    }
+    
+    
+    
+        //somar = (matriz[0][0] + matriz [1][1]+ matriz[2][2]);
+        //somar += (matriz[0][2] + matriz [1][1]+ matriz[2][0]);
+    
+    printf("\n %d",somar);
+
+
+    return 0;
+}
