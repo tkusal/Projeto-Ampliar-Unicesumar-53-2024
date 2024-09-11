168c891cd4df12511f0f5d2703046a7f18465a18
tkusal <thiago.kusal@outlook.com>
2024-09-10 22:39:26 -0300

    Aula 10/09/2024

--- Semana 8/Atv1 - Desafio Para Casa.c
+++ Semana 8/Atv1 - Desafio Para Casa.c
@@ -1,22 +1,7 @@
 /*
-    Escreva um programa que cadastre até 5 clientes e até 5 produtos.
-    No sistema o usuário deve poder:
-
-    - Cadastrar o cliente (Código, Nome, idade, saldo)
-    - Cadastrar produto (código do produto, descrição do produto, valor do produto, qtd)
-    - Cadastrar uma venda de um produto pra um cliente (pelo código do cliente e código do produto).
-    - Listar todas as vendas
-    - Listar todas os cliente
-
-    - Use SUBROTINAS e PONTEIROS onde julgar necessário
-
-    Cadastro de venda:
-    - Recebe o código do cliente e valida se o cliente existe
-    - Recebe o código do produto e valida se o produto existe
-    - Recebe a quantidade de produto da venda e valida se existe em estoque
-    - Calcula o total da Venda
-    - Valida se o cliente tem saldo pra finalizar a compra
-    - Abate o valor da venda no saldo do cliente e a quantidade vendida no estoque do produto
+    Garantir que o sistema utilize as subrotinas de ValidarProduto e validarCLiente
+    para evitar que o usuário cadastre dois/mais clientes ou dois/mais produtos com
+    o mesmo código.
 */
 
 #include <stdio.h>
